cd1ff231053c677d8aeb4a4d24052a40ade48d8b
Jack Doyle <147415482+jackdoylejackdoyle@users.noreply.github.com>
2023-10-09 16:52:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,2 @@
 import streamlit as st
-import pandas as pd
-import seaborn as sns
-
-st.write("Presentation of Dataset US Housing")
-
-housing = pd.read_csv("USA_Housing.csv")
-st.dataframe(housing)
-sns.jointplot(data=housing, x="Avg. Area House Age", y="Price")
+st.write("Hello world")
